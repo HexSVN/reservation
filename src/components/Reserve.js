6c617c53e822fd8049c0563cdac722a43e84f0a2
HexSVN <haxerwangyi@163.com>
2017-06-28 16:39:02 +0800

    tmp

---
+++ src/components/Reserve.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wafer on 2017/6/28.
+ */
+import React from 'react';
+
+export default ({dispath}) =>{
+  return(
+    <div>
+      <h1>This is Reserve component</h1>
+      <hr/>
+    </div>
+  );
+}
