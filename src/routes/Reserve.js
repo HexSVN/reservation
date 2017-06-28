8152e0ba3e25260af477d11f5077ffa7f7482886
HexSVN <haxerwangyi@163.com>
2017-06-28 17:55:22 +0800

    tmp

--- src/routes/Reserve.js
+++ src/routes/Reserve.js
@@ -2,9 +2,8 @@
  * Created by wafer on 2017/6/28.
  */
 import React from 'react';
-import {connect} from 'dva';s
+import {connect} from 'dva';
 import ReserveComponent from '../components/Reserve';
-
 const Reserve = () =>{
   return(
     <ReserveComponent/>
